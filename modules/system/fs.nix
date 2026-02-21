094b4cfeec54c3557dc65fc72da7b7497ce7e8f7
lemonisniket <chernovyaroslav228@gmail.com>
2026-02-21 07:07:27 +0300

    system: modules: fs: little syntax fixes

--- modules/system/fs.nix
+++ modules/system/fs.nix
@@ -1,36 +1,38 @@
-{
+{ lib, ...}:
+
   let 
     commonBtrfsOptions = [ "noatime" "compress=zstd:3" "ssd" "discard=async" "space_cache=v2" ];
     rootDevice = "/dev/nvme0n1p2";
     bootDevice = "/dev/nvme0n1p1";
   in
+{
   fileSystems = {
     "/" = { 
-      device = rootDevice;
+      device = lib.mkForce rootDevice;
       fsType = "btrfs";
       options = [ "subvol=@nixos" ] ++ commonBtrfsOptions;
     };
 
     "/nix" = { 
-      device = rootDevice; 
+      device = lib.mkForce rootDevice; 
       fsType = "btrfs"; 
       options = [ "subvol=@nix" ] ++ commonBtrfsOptions; 
     };
 
     "/home" = { 
-      device = rootDevice; 
+      device = lib.mkForce rootDevice; 
       fsType = "btrfs"; 
       options = [ "subvol=@home" "nobarrier" "commit=60" ] ++ commonBtrfsOptions; 
     };
 
     "/var/log" = {
-      device = rootDevice;
+      device = lib.mkForce rootDevice;
       fsType = "btrfs";
       options = [ "subvol=@nixos" "noatime" "compress=zstd:5" "nobarrier" ];
     };
 
     "/boot" = {
-      device = bootDevice;
+      device = lib.mkForce bootDevice;
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
