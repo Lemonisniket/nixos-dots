7830747caf2db9dfb69c28a42f15f8faab936482
lemonisniket <chernovyaroslav228@gmail.com>
2026-02-21 08:05:23 +0300

    system: modules: nvidia: downgrade to 580.126.09

--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -2,10 +2,10 @@
 
 let
   nvidia_580 = config.boot.kernelPackages.nvidia_x11.overrideAttrs (oldAttrs: rec {
-    version = "580.126.18";
+    version = "580.126.09";
     src = pkgs.fetchurl {
       url = "https://download.nvidia.com/XFree86/Linux-x86_64/${version}/NVIDIA-Linux-x86_64-${version}.run";
-      sha256 = "sha256-p3gbLhwtZcZYCRTHbnntRU0ClF34RxHAMwcKCSqatJ0=";
+      sha256 = "sha256-TKxT5I+K3/Zh1HyHiO0kBZokjJ/YCYzq/QiKSYmG7CY=";
     };
   });
 in
