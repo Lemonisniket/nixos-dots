0aa70ed26fb868f6890b27ff57fbd8dca86acd15
lemonisniket <chernovyaroslav228@gmail.com>
2026-02-21 06:06:09 +0300

    system: modules: virt: disable docker start on boot

--- modules/system/virt.nix
+++ modules/system/virt.nix
@@ -1,8 +1,11 @@
 { ... }: {
   virtualisation.docker = {
     enable = true;
+    enableOnBoot = false;
     listenOptions = [ "/var/run/docker.sock" ];
   };
+
+  systemd.sockets.docker.wantedBy = [ "sockets.target" ];
 
   virtualisation.libvirtd.enable = true;
   virtualisation.spiceUSBRedirection.enable = true;
