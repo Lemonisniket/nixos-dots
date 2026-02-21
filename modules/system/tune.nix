d62e9dd7233f87d3fb5fc829564e556ac618dc93
lemonisniket <chernovyaroslav228@gmail.com>
2026-02-21 21:36:05 +0300

    system: modules: tune: some little sysctl parameters

--- modules/system/tune.nix
+++ modules/system/tune.nix
@@ -9,14 +9,22 @@
   };
 
   boot.kernel.sysctl = {
-    "vm.swappiness" = 50;
+    "vm.swappiness" = 140;
     "vm.page-cluster" = 0;
+    
     "vm.dirty_ratio" = 10;
     "vm.dirty_background_ratio" = 5;
+    "vm.dirty_expire_centisecs" = 500;
+    "vm.dirty_writeback_centisecs" = 100;
+
+    "vm.vfs_cache_pressure" = 50;
 
     "net.ipv4.ip_forward" = 1;
     "net.ipv4.conf.all.forwarding" = 1;
     "net.core.default_qdisc" = "fq";
     "net.ipv4.tcp_congestion_control" = "bbr";
+    "net.ipv4.tcp_fastopen" = 3;
+    
+    "kernel.sched_autogroup_enabled" = 1;
   };
 }
