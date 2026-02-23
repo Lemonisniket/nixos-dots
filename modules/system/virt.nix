33f6ddbeb122e1788346f50719cc9f569f5bda10
lemonisniket <chernovyaroslav228@gmail.com>
2026-02-23 13:39:31 +0300

    system: modules: virt: increase docker service limits

--- modules/system/virt.nix
+++ modules/system/virt.nix
@@ -3,6 +3,11 @@
     enable = true;
     enableOnBoot = false;
     listenOptions = [ "/var/run/docker.sock" ];
+    extraOptions = "--default-ulimit nofile=65536:1048576";
+  };
+
+  systemd.services.docker.serviceConfig = {
+    LimitNOFILE = 1048576;
   };
 
   systemd.sockets.docker.wantedBy = [ "sockets.target" ];
